a41f639c882cec820fbeed9c98e464c98ac6025b
JoaoPauloQC <joaopauloqueirozcosta@gmail.com>
2025-05-19 20:40:12 -0300

    Adição do funcionamento do banco de dados

--- projeto_cad_school/app_cad_school/views.py
+++ projeto_cad_school/app_cad_school/views.py
@@ -5,11 +5,20 @@ from .models import Aluno , Materia , Notas
 def home(request):
     return render(request,"usuarios/login.html")
 
+def error(request):
+    return render(request, 'usuarios/erro.html')
+
 def login_action(request):
     nome_got = request.POST.get('nome')
-    usuario_logado = Aluno.objects.filter(nome= nome_got )
+    print(nome_got)
+    usuario_logado = Aluno.objects.get( nome = nome_got )
+    print(usuario_logado.nome)
     notas = {
-        Notas.objects.filter(aluno=usuario_logado.nome)
+        'notas' : Notas.objects.filter(aluno = usuario_logado)
         }
+    
     #Retornar os dados para a pagina de listafem de usuarios
-    return render(request , 'usuarios/notas.html' , notas)
+    if notas :
+        return render(request , 'usuarios/notas.html' , notas)
+    else:
+        return error()
